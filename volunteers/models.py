39851e38fb6e1d7b9b5819dcffc5ded7100fade9
Erin Verbeck <erin@merginglanes.net>
2019-06-22 15:58:07 -0500

    Create models in models.py for signup form

--- volunteers/models.py
+++ volunteers/models.py
@@ -1,16 +1,7 @@
 from django.db import models
 from django.core.validators import MinLengthValidator
+from django.forms import ModelForm
 # Create your models here.
-
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
 
 class Volunteer(models.Model):
     last_name = models.CharField(max_length=30, validators=[MinLengthValidator(1, "Please enter a valid name.")])
