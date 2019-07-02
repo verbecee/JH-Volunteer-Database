f75b64825079fa02b41cd95023b5e57383782b75
Erin Verbeck <erin@merginglanes.net>
2019-07-01 19:28:10 -0500

    WIP

--- volunteers/views.py
+++ volunteers/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.form import UserCreationForm
 # Create your views here.
-def index(req):
-    return render(req, 'volunteers/index.html')
+def index(request):
+    return render(request, 'volunteers/index.html')
+
+def signup(request):
+    return render(request, 'volunteers/signup_form.html')
+
