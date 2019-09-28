aa5f6f221eb3e50b3dc12fe8ced51324bc0583bf
Erin Verbeck <erin@merginglanes.net>
2019-09-28 12:10:45 -0500

    link start page to application form and add in text and button

--- volunteers/urls.py
+++ volunteers/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('apply', views.vol_application, name='volunteer'),
+    path('application', views.vol_application, name='application'),
+    path('apply', views.vol_start, name='start'),
     path('ideas', views.ideas, name='ideas'),
+
     # path('apply', views.create_application, name='createapplication'),
 ]
